aa079c88b9a7a3d99f36e7ec680fe35453cd1fb2
Christopher Miller <admin@mctherealm.net>
2023-03-10 17:19:58 -0800

    protocol

---
+++ shared/src/protocol.rs
@@ -0,0 +1,13 @@
+use std::time::Duration;
+
+use naia_bevy_shared::{LinkConditionerConfig, Protocol};
+
+// Protocol Build
+pub fn protocol() -> Protocol {
+  Protocol::builder()
+    // Config
+    .tick_interval(Duration::from_millis(16))
+    .link_condition(LinkConditionerConfig::new(100, 0, 0.0))
+    .enable_client_authoritative_entities()
+    .build()
+}
